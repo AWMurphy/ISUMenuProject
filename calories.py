ca0f96d95c7b8bc7a7a626db1eac6f139e524545
Ben10lightning <bennowell@icloud.com>
2024-10-03 16:37:26 -0500

    made calories.py get all sections of menu

--- calories.py
+++ calories.py
@@ -5,22 +5,32 @@ from bs4 import BeautifulSoup
 
 
 driver = webdriver.Edge()  # or webdriver.Firefox()
-driver.get("https://www.dining.iastate.edu/location/friley-windows-2-2/")
-element = driver.find_element("xpath", '/html/body/div/div/main/div/article/div/section/div[2]/div/div/div[2]/div/div/div[4]/div[8]/div[1]')
-elementHTML = element.get_attribute('innerHTML')
+driver.get("https://www.dining.iastate.edu/location/whirlybird")
 
-soup = BeautifulSoup(elementHTML, 'lxml')
+index = 6
 
-# Extract specific information, e.g., menu items, calories, and categories
-items = soup.find_all('li', class_='mh-location--single-menu--item')
-for item in items:
-    name = item.find('span', class_='mh-menu-item-name').text
+while True:
     try:
-        calories = item.find('span', class_='mh-menu-item-calories').text
+        element = driver.find_element("xpath", '//*[@id="hours-and-menus"]/div/div[2]/div/div/div[4]/div[' + str(index) + ']') #xpath is to the visible menu, need to click on lunch and dinner to get those to work
+        elementHTML = element.get_attribute('innerHTML')
+
+        soup = BeautifulSoup(elementHTML, 'lxml')
+
+        # Extract specific information, e.g., menu items, calories, and categories
+        items = soup.find_all('li', class_='mh-location--single-menu--item')
+        for item in items:
+            name = item.find('span', class_='mh-menu-item-name').text
+            try:
+                calories = item.find('span', class_='mh-menu-item-calories').text
+            except:
+                calories = None
+            categories = item.find_all('span', class_='mh-menu-item-category')
+            category_list = [category['aria-label'] for category in categories]
+            
+            print(f'Item: {name}, Calories: {calories}, Categories: {category_list}')
     except:
-        calories = None
-    categories = item.find_all('span', class_='mh-menu-item-category')
-    category_list = [category['aria-label'] for category in categories]
-    
-    print(f'Item: {name}, Calories: {calories}, Categories: {category_list}')
+        break
+    index += 1
+    print("fuck me")
+
 driver.quit()
--- test.py
+++ test.py
@@ -5,22 +5,10 @@ from bs4 import BeautifulSoup
 
 
 driver = webdriver.Edge()  # or webdriver.Firefox()
-driver.get("https://www.dining.iastate.edu/location/friley-windows-2-2/")
-element = driver.find_element("xpath", '/html/body/div/div/main/div/article/div/section/div[2]/div/div/div[2]/div/div/div[4]/div[8]/div[1]')
-elementHTML = element.get_attribute('innerHTML')
+driver.get("https://www.dining.iastate.edu/hours-menus/")
+time.sleep(4)
+continue_link = driver.find_element(By.LINK_TEXT, 'Memorial Union Food')
+driver.execute_script("arguments[0].scrollIntoView();", continue_link)
+continue_link.click()
+time.sleep(2)
 
-soup = BeautifulSoup(elementHTML, 'lxml')
-
-# Extract specific information, e.g., menu items, calories, and categories
-items = soup.find_all('li', class_='mh-location--single-menu--item')
-for item in items:
-    name = item.find('span', class_='mh-menu-item-name').text
-    try:
-        calories = item.find('span', class_='mh-menu-item-calories').text
-    except:
-        calories = None
-    categories = item.find_all('span', class_='mh-menu-item-category')
-    category_list = [category['aria-label'] for category in categories]
-    
-    print(f'Item: {name}, Calories: {calories}, Categories: {category_list}')
-driver.quit()
