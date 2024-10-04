6952bf6ddf3d53f9dc983288a7b7eba11bc1821d
Ben10lightning <bennowell@icloud.com>
2024-10-03 23:09:17 -0500

    did a bunch of shit

--- calories.py
+++ calories.py
@@ -4,33 +4,38 @@ import time
 from bs4 import BeautifulSoup
 
 
-driver = webdriver.Edge()  # or webdriver.Firefox()
-driver.get("https://www.dining.iastate.edu/location/whirlybird")
+#driver = webdriver.Edge()  # or webdriver.Firefox()
 
-index = 6
 
-while True:
-    try:
-        element = driver.find_element("xpath", '//*[@id="hours-and-menus"]/div/div[2]/div/div/div[4]/div[' + str(index) + ']') #xpath is to the visible menu, need to click on lunch and dinner to get those to work
-        elementHTML = element.get_attribute('innerHTML')
+def PrintMenus(Driver, link):
+    index = 6
 
-        soup = BeautifulSoup(elementHTML, 'lxml')
+    driver = Driver
 
-        # Extract specific information, e.g., menu items, calories, and categories
-        items = soup.find_all('li', class_='mh-location--single-menu--item')
-        for item in items:
-            name = item.find('span', class_='mh-menu-item-name').text
-            try:
-                calories = item.find('span', class_='mh-menu-item-calories').text
-            except:
-                calories = None
-            categories = item.find_all('span', class_='mh-menu-item-category')
-            category_list = [category['aria-label'] for category in categories]
-            
-            print(f'Item: {name}, Calories: {calories}, Categories: {category_list}')
-    except:
-        break
-    index += 1
-    print("fuck me")
+    driver.get(link)
 
-driver.quit()
+    failed = False
+    while (failed == False):
+        try:
+            element = driver.find_element("xpath", '//*[@id="hours-and-menus"]/div/div[2]/div/div/div[4]/div[' + str(index) + ']') #xpath is to the visible menu, need to click on lunch and dinner to get those to work
+            elementHTML = element.get_attribute('innerHTML')
+
+            soup = BeautifulSoup(elementHTML, 'lxml')
+
+            # Extract specific information, e.g., menu items, calories, and categories
+            items = soup.find_all('li', class_='mh-location--single-menu--item')
+            for item in items:
+                name = item.find('span', class_='mh-menu-item-name').text
+                try:
+                    calories = item.find('span', class_='mh-menu-item-calories').text
+                except:
+                    calories = None
+                categories = item.find_all('span', class_='mh-menu-item-category')
+                category_list = [category['aria-label'] for category in categories]
+                
+                print(f'Item: {name}, Calories: {calories}, Categories: {category_list}')
+        except:
+            failed = True
+        index += 1
+        print(" ")
+
